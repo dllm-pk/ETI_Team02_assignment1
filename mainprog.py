5749284bb852d87371a26910f210e9c57cc0f580
Say Jin Thong <s10177191@connect.np.edu.sg>
2020-12-10 10:47:53 +0800

    minor edit (Faris using Vic's comp)

--- mainprog.py
+++ mainprog.py
@@ -10,7 +10,7 @@ user_hp = 100
 #Menu for combat
 combat_mode = True #Ensures loop functionality
 while combat_mode == True: #Loop for combat menu
-    print("Encounter! - Rat \nDamage: {0} - {1} \nDefence: {2} \nHP: {3} \n1) Attack \n2) Run".format(min,max,defn,hp))
+    print("Encounter! - Rat \nDamage: {0} - {1} \nDefence: {2} \nHP: {3} \n\n1) Attack \n2) Run".format(min,max,defn,hp))
     choice = input("Enter Choice: ")#Menu choice
     print(" ")
     if choice == "1":#Attack
@@ -22,7 +22,7 @@ while combat_mode == True: #Loop for combat menu
             break#End combat
         else:
             tackle = combat.enemy_attack()#damage to player
-            print("Ouch! The Rat hit you for {} damage!".format(tackle))
+            print("Ouch! The Rat hit you for {} damage! \n".format(tackle))
             user_hp = user_hp - tackle
             if user_hp <= 0:
                 user_hp = 0#player's HP cant go below 0
