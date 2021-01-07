40304348f4626e14c79e28774f7be982cfbc3b71
Say Jin Thong <s10177191@connect.np.edu.sg>
2021-01-07 20:52:43 +0800

    combat function test renamed

---
+++ test_combat.py
@@ -0,0 +1,14 @@
+
+import pytest
+from full_program import *
+
+class test_main(unittest.TestCase) :
+
+    
+    def test_attack(min_damage,max_damage, defence):
+        add(1)
+        assert "You dealt 10 damange to the rat"
+
+    def test_flee():
+        value=add(b)
+        assert value == result
--- test_main.py
+++ test_main.py
@@ -6,10 +6,6 @@ import combat
 class test_main(unittest.TestCase) :
 
     @pytest.mark.parameterize()
-    def test_attack(min_damage,max_damage, defence):
-        lose_hp = random.randint(min_damage,max_damage) - defence
-        return lose_hp
-
-    def test_flee():
-        value=input(b)
-        assert value == 'You fled'
+    def test_town_menu():
+        Enter(1)
+        assert 
