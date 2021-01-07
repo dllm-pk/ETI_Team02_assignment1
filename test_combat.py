e677f4907ac229333ff323e30b35444e6571ea1d
Say Jin Thong <s10177191@connect.np.edu.sg>
2021-01-07 21:05:22 +0800

    idk why

--- test_combat.py
+++ test_combat.py
@@ -1,14 +1,12 @@
 
 import pytest
-from full_program import *
 
-class test_main(unittest.TestCase) :
-
-    
-    def test_attack(min_damage,max_damage, defence):
+@pytest.mark.one   
+def test_attack():
         add(1)
         assert "You dealt 10 damange to the rat"
 
-    def test_flee():
-        value=add(b)
-        assert value == result
+def test_flee():
+    add(2)
+    assert "You fled"
+    
