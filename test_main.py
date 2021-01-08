6ed32277aa05d932279b882f4e88efdcb288b82f
Say Jin Thong <s10177191@connect.np.edu.sg>
2021-01-08 14:26:39 +0800

    Again, idk why

--- test_main.py
+++ test_main.py
@@ -1,11 +1,11 @@
 import pytest
-import mainprog
-import combat 
 
+@pytest.mark.parametrize('user_input', ["1","2","3","4","5","6" ])
+def test_enter_town():
+    TOWN_OPTIONS= int(user_input)
+    assert choie == TOWN_OPTIONS
 
-class test_main(unittest.TestCase) :
+      
+       
 
-    @pytest.mark.parameterize()
-    def test_town_menu():
-        Enter(1)
-        assert 
+        
