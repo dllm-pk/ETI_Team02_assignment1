721b14d725b7d8627423fa41f5fd451705406111
Say Jin Thong <s10177191@connect.np.edu.sg>
2021-01-13 20:32:23 +0800

    Added test_view character

--- test_combat.py
+++ test_combat.py
@@ -1,12 +1,14 @@
 
 import pytest
+from full_program import *
 
 @pytest.mark.one   
-def test_attack():
-        add(1)
-        assert "You dealt 10 damange to the rat"
+def test_attack(outdoor_attack):
+    add(1)
+    option = get_option(1)
+    COMBAT_OPTIONS[option-1](game)
 
-def test_flee():
+def test_flee(outdoor_run):
     add(2)
     assert "You fled"
     
--- test_main.py
+++ test_main.py
@@ -1,10 +1,18 @@
 import pytest
+from full_program import *
 
-@pytest.mark.parametrize('user_input', ["1","2","3","4","5","6" ])
+@pytest.mark.parametrize()
 def test_enter_town():
-    TOWN_OPTIONS= int(user_input)
-    assert choie == TOWN_OPTIONS
+    TOWN_OPTIONS= int(1)
+    assert "You are in a town"
 
+def test_view_character(game):
+    hero = game[HERO_INDEX]
+    
+    assert("The Hero")
+    assert("  Damage:", hero, "-", hero)
+    assert(" Defence:", hero)
+    assert("      HP:", hero)
       
        
 
