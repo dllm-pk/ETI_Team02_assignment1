1ba4fe284d58d0ba283a21c553341f093b685a59
faris122 <fi2955962@gmail.com>
2021-01-29 08:37:04 +0800

    Closed #52

--- menu.py
+++ menu.py
@@ -1,34 +1,37 @@
 from mainprog import *
+from class1 import *
+import random
 #Faris: Manual test
 #Test values
-min = 1
-max = 3
-defn = 1
-hp = 10
-maxuserhp = 100
-user_hp = 50
-user_defence = 1
+char = characterinfo("The Hero",random.randint(30,50),random.randint(40,60),random.randint(10,20),100,False,100)
+rat = enemyinfo("Rat",random.randint(10,20),random.randint(20,30),random.randint(1,5),100,100)
+ratking = enemyinfo("Rat King",random.randint(30,50),random.randint(40,60),random.randint(10,20),100,100)
 day = 1
 
 #Menu for testing rest (Faris)
 testmenu = True
 while testmenu == True:
     print("Today is Day " +str(day))
-    print("Your stats. Damage: {0}-{1} HP:{2} Defence {3}".format(min,max,user_hp,user_defence))
-    a = input("(1) rest (2) combat (3) Orb of Power (0) stop test: ")
+    print("Your stats.")
+    char.toString
+    a = input("(1) rest (2) combat (3) Orb of Power (4) View Character (0) stop test: ")
     if a == "1":
-        restresult = rest(day,user_hp,maxuserhp)
+        restresult = rest(day,char)
         day = restresult[0]
-        user_hp = restresult[1]
+        char.hp = restresult[1]
     elif a == "2":
-        user_hp = enemy(min,max,defn,hp,user_hp,maxuserhp,user_defence)
+        char.hp = enemy(char,rat)
     elif a == "3":
-        orb = orb(min,max,user_defence)
-        min = orb[0]
-        max = orb[1]
-        user_defence = orb[2]
+        orb = orb(char)
+        char.minDamage = orb[0]
+        char.maxDamage = orb[1]
+        char.defence = orb[2] 
+        OOP = True
+    elif a == "4":
+        char.toString
     elif a == "0":
         testmenu = False
         break
     else:
         print("Invalid Choice")
+
--- test_combat.py
+++ test_combat.py
@@ -1,34 +1,27 @@
-
+from class1 import *
 import pytest
 import random
-#from full_program import *
-
-
-#def test_attack(outdoor_attack):
-    #add(1)
-    #option = get_option(1)
-    #COMBAT_OPTIONS[option-1](game)
-
-#def test_enemyattack(outdoor_run):
-    #add(2)
-    #assert "You fled"
-
 from combat import *
 def test_playerattack_notzero():
-    a = random.randint(1,10) #min dmg
-    b = random.randint(a+1,a+4) #max dmg
-    c = random.randint(1,a-1) #defence 
-    value = attack(a,b,c)
-    max = b-c
-    min = a-c
+    char = characterinfo("The Hero",random.randint(30,50),random.randint(40,60),random.randint(10,20),100,False,100)
+    rat = enemyinfo("Rat",random.randint(10,20),random.randint(20,30),random.randint(1,5),100,100)
+    value = attack(char,rat)
     assert value > 0 #Value never below 0
 def test_playerattack_belowmax():
-    a = random.randint(1,10) #min dmg
-    b = random.randint(a+1,a+4) #max dmg
-    c = random.randint(1,a-1) #defence 
-    value = attack(a,b,c)
-    max = b-c
-    min = a-c
-    assert value <= max #Value always lower than max
+    char = characterinfo("The Hero",random.randint(30,50),random.randint(40,60),random.randint(10,20),100,False,100)
+    rat = enemyinfo("Rat",random.randint(10,20),random.randint(20,30),random.randint(1,5),100,100)
+    value = attack(char,rat)
+    assert value <= char.maxDamage #Value always lower than max
+def test_attack_rat_king_noOOP():
+    char = characterinfo("The Hero",random.randint(30,50),random.randint(40,60),random.randint(10,20),100,False,100)
+    ratking = enemyinfo("Rat King",random.randint(30,50),random.randint(40,60),random.randint(10,20),100,100)
+    value = ratkingattack(char,ratking)
+    assert value == 0
+def test_attack_rat_king_OOP():
+    char = characterinfo("The Hero",random.randint(30,50),random.randint(40,60),random.randint(10,20),100,True,100)
+    ratking = enemyinfo("Rat King",random.randint(30,50),random.randint(40,60),random.randint(10,20),100,100)
+    value = ratkingattack(char,ratking)
+    assert value != 0
+
 
     
